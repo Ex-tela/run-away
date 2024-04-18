7a8ffdbb79f68d04e69ee58662a814a51be11d75
Estela Valle <dvmestela@gmail.com>
2024-04-18 17:08:40 +0100

    navbar, footer and center content

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,18 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite/plugin"
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/dist/**/*.cjs",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [flowbite],
+  // darkMode: 'selector',
+  // darkMode: ['variant', '&:not(.light *)']
 };
 export default config;
